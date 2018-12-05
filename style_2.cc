9f5e29858d8af9a642d8f60c1c279ddbec52be64
joweeet <37391709+Joweet@users.noreply.github.com>
2018-12-04 16:10:15 -0800

    portfolio html addition

---
+++ style_2.cc
@@ -0,0 +1,21 @@
+.optionA {
+    bottom: 0;
+    left: 0;
+    position: fixed;
+    background-color: white;
+    padding: 10px;
+}
+.optionB {
+    bottom: 0;
+    left: 0;
+    position: fixed;
+    background-color: white;
+    padding: 10px;
+}
+.optionC {
+    bottom: 0;
+    left: 0;
+    position: fixed;
+    background-color: white;
+    padding: 10px;
+}
